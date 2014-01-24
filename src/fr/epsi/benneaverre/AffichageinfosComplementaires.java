7bfbf9fad2b5c08205d93bb3e4c6149a5dc0a011
JihairS <jeanromainsignorelli@gmail.com>
2014-01-24 15:34:15 +0100

    Synch images

---
+++ src/fr/epsi/benneaverre/AffichageinfosComplementaires.java
@@ -0,0 +1,13 @@
+package fr.epsi.benneaverre;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AffichageinfosComplementaires extends Activity {
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.infos_comp);
+    } 
+}
